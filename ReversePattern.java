5a3cc73ecb2ecb95529d9fb46a43ee73564a28d6
kohlisneha <Snehakohli01@gmail.com>
2023-12-23 14:08:46 +0530

    Added patterns

---
+++ ReversePattern.java
@@ -0,0 +1,12 @@
+public class ReversePattern{
+    public static void main(String a[]){
+        for(int i=5;i>0;i--){
+            for(int j=0;j<i;j++){
+                System.out.print("*");
+
+            }
+        
+        System.out.println();
+    }
+    }
+}
